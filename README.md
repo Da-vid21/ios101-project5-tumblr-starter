6f730e090e89c69e5eee3664ecf5a14a000c435f
David Tekeste <tekestede@vcu.edu>
2024-03-25 20:35:00 -0400

    Done

---
+++ README.md
@@ -0,0 +1,54 @@
+# Project 5 - *Tumblr API Client*
+
+Submitted by: **Dawit Tekeste**
+
+**Tumblr API Client** is an app that ... [TODO] 
+
+Time spent: **2** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [X] App has a configured table view and table view call
+- [X] App populates the table view with data fetched from an API
+
+
+The following **optional** features are implemented:
+
+- [ ] App fetches posts from a different Tumblr blog
+- [X] App has a refresh control to update the table view
+
+The following **additional** features are implemented:
+
+- [ ] List anything else that you can get done to improve the app functionality!
+
+## Video Walkthrough
+
+<div>
+    <a href="https://www.loom.com/share/db8e45e2c6ea428db648dcdfab61ca91">
+      <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/db8e45e2c6ea428db648dcdfab61ca91-with-play.gif">
+    </a>
+  </div>
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+* I had a problem trying to figure out where to delete constrains because my dumb ass named the same UITableView Identifier the UITableView and it caused runtime errors
+
+## License
+
+    Copyright [2024] [Dawit Tekeste]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,21 +6,50 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private var posts: [Post] = []
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        
+        // Set up refresh control
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+        tableView.refreshControl = refreshControl
         
         fetchPosts()
     }
+    
+    @objc private func refreshData(_ sender: Any) {
+        fetchPosts()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        
 
-
+        let post = posts[indexPath.row]
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            Nuke.loadImage(with: url, into: cell.postImageView)
+        }
+        cell.label.text = post.summary
+        return cell
+    }
+    
     func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
-        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+        
+        let session = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+
             if let error = error {
                 print("❌ Error: \(error.localizedDescription)")
                 return
@@ -30,7 +59,7 @@ class ViewController: UIViewController {
                 print("❌ Response error: \(String(describing: response))")
                 return
             }
-
+            
             guard let data = data else {
                 print("❌ Data is NIL")
                 return
@@ -38,22 +67,17 @@ class ViewController: UIViewController {
 
             do {
                 let blog = try JSONDecoder().decode(Blog.self, from: data)
-
-                DispatchQueue.main.async { [weak self] in
-
-                    let posts = blog.response.posts
-
-
-                    print("✅ We got \(posts.count) posts!")
-                    for post in posts {
-                        print("🍏 Summary: \(post.summary)")
-                    }
+                DispatchQueue.main.async {
+                    self.posts = blog.response.posts
+                    self.tableView.reloadData()
+                    self.tableView.refreshControl?.endRefreshing()
+                    print("✅ We got \(self.posts.count) posts!")
                 }
-
             } catch {
                 print("❌ Error decoding JSON: \(error.localizedDescription)")
             }
         }
+        
         session.resume()
     }
 }
